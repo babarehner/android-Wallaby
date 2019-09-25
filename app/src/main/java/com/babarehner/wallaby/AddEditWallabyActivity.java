006fc34aa7c6274323e74ebd8231eb183847de05
mike <babarehner@gmail.com>
2019-09-25 11:33:32 -0400

    Subject: Begin AddEditWallabyActivity
    What Happened: start implementing AddEditWallabyActivity. This should not be done on the master track.

---
+++ app/src/main/java/com/babarehner/wallaby/AddEditWallabyActivity.java
@@ -0,0 +1,61 @@
+package com.babarehner.wallaby;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+
+/**
+ * Project Name: Wallaby
+ * <p>
+ * Copyright 9/25/19 by Mike Rehner
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * Adds and edits items in the Wallaby database. Each item consists of a Picture and a Name
+ */
+
+public class AddEditWallabyActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_wallaby);
+
+        Intent intent = getIntent();
+        // mCurrentWallabyUri = intent.getData();
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+}
